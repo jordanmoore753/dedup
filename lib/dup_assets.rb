a706170bc09091c1bb579b798e71aeecba8de70b
Jordan Moore <jordanmoore753@gmail.com>
2026-08-16 13:07:30 -0400

    rb: move default root to lib

--- lib/dup_assets.rb
+++ lib/dup_assets.rb
@@ -2,7 +2,11 @@ require_relative "dup_assets/finder"
 require_relative "dup_assets/reporter"
 
 module DupAssets
-  def self.call(root)
+  DEFAULT_ROOT = "app/assets"
+
+  private_constant :DEFAULT_ROOT
+
+  def self.call(root = DEFAULT_ROOT)
     groups = Finder.call(root)
     Reporter.call(groups)
   end
