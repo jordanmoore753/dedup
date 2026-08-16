a401d2d2c95bce4b8b7832c8247393d7271911a4
Jordan Moore <jordanmoore753@gmail.com>
2026-08-16 12:52:11 -0400

    rb: move finding logic to finder module

---
+++ lib/dup_assets/finder.rb
@@ -0,0 +1,25 @@
+require "digest"
+require "find"
+
+module DupAssets
+  module Finder
+    ASSET_EXTENSIONS = %w[.png .jpg .jpeg .gif .svg .ico .webp].freeze
+
+    private_constant :ASSET_EXTENSIONS
+
+    def self.call(root)
+      by_hash = Hash.new { |h, k| h[k] = [] }
+
+      Find.find(root) do |path|
+        next if File.symlink?(path)
+        next unless File.file?(path)
+        next unless ASSET_EXTENSIONS.include?(File.extname(path).downcase)
+
+        hash = Digest::SHA256.file(path).hexdigest
+        by_hash[hash] << path
+      end
+
+      by_hash.values.select { |paths| paths.size > 1 }
+    end
+  end
+end
